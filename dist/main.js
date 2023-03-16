718022625f07b46a79e03beba3714d38b32dcad4
Ruben Christoffer Hegland-Antonsen <ruben.hegland-antonsen@tietoevry.com>
2023-03-16 15:02:14 +0100

    Got basic insert working

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,30 @@
 "use strict";
-console.log("running");
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
+async function main() {
+    // ... you will write your Prisma Client queries here
+    prisma.user.create({
+        data: {
+            name: 'Alice',
+            email: 'alice@prisma.io',
+            posts: {
+                create: { title: 'Hello World' },
+            },
+            profile: {
+                create: { bio: 'I like turtles' },
+            },
+        }
+    }).then(res => {
+        console.log(res);
+    });
+}
+main()
+    .then(async () => {
+    await prisma.$disconnect();
+})
+    .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+});
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,35 @@
-console.log("running");
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    // ... you will write your Prisma Client queries here
+
+    prisma.user.create({
+        data: {
+            name: 'Alice',
+            email: 'alice@prisma.io',
+            posts: {
+                create: { title: 'Hello World' },
+            },
+            profile: {
+                create: { bio: 'I like turtles' },
+            },
+        }
+    }).then(res => {
+        console.log(res);
+    });
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+
+    .catch(async (e) => {
+        console.error(e)
+
+        await prisma.$disconnect()
+
+        process.exit(1)
+    });
